baa741da16dff22392a1648dde24748b2f14dfbd
edwardjcw <edwardjcw@live.com>
2017-03-08 16:43:06 -0600

    added start reading from clipboard immediately command line argument

--- AutoReader/MainWindow.xaml.cs
+++ AutoReader/MainWindow.xaml.cs
@@ -21,9 +21,41 @@ namespace AutoReader
 
             m_Synth = new SpeechSynthesizer();
             m_Synth.SetOutputToDefaultAudioDevice();
+
+            if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1] == "1")
+            {
+                m_AutoPlayToggle.Visibility = Visibility.Hidden;
+                m_AutoSaveToggle.Visibility = Visibility.Hidden;
+                m_PasteButton.Visibility = Visibility.Hidden;
+                m_TextBox.IsEnabled = false;
+                m_Synth.StateChanged += SynthOnStateChangedStartup;
+                OnClickClearAndPaste(this, new RoutedEventArgs());
+            }
+
             m_Synth.StateChanged += SynthOnStateChanged;
             m_Information.Content = "Ready.";
 
+        }
+
+        private void SynthOnStateChangedStartup(object sender, StateChangedEventArgs stateChangedEventArgs)
+        {
+            switch (stateChangedEventArgs.State)
+            {
+                case SynthesizerState.Speaking:
+                    m_Information.Content = "Speech in Process";
+                    m_Button.Content = "Stop";
+                    
+                    return;
+                case SynthesizerState.Paused:
+                    break;
+                case SynthesizerState.Ready:
+                    m_Information.Content = "Done. Ready.";
+                    m_Button.Content = "Start";
+                    Application.Current.Shutdown();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         private void Play()
